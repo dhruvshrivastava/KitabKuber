999933e8b3fa5b82030f7033a11ae0d4fbc2c0b2
Dhruv Shrivastava <dhruvsri5@gmail.com>
2020-09-26 19:57:41 +0530

    Update urls.py

--- kitabkuber/urls.py
+++ kitabkuber/urls.py
@@ -23,6 +23,3 @@ urlpatterns = [
     path('', include('catalog.urls')),
    
 ]
-if settings.DEBUG: 
-        urlpatterns += static(settings.MEDIA_URL, 
-                              document_root=settings.MEDIA_ROOT) 
