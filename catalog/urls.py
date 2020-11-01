92713148db36de0a9d95e5c59c389f3b9d033d4d
Dhruv Shrivastava <dhruvsri5@gmail.com>
2020-11-01 14:07:08 +0530

    Bug

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path 
-from .views import Home, bookdetail, sell, order, search, categories, rental, contact, checkout
+from .views import Home, bookdetail, sell, order, search, categories, rental, contact, checkout, faq
 from django.conf.urls.static import static
 from django.conf import settings 
 
