4663ebbd5b076defad47c320603c8a93652d0b58
Dhruv Shrivastava <dhruvsri5@gmail.com>
2020-12-01 12:12:50 +0530

    Update: Zero Cost Rental Plan

--- kitabkuber/settings.py
+++ kitabkuber/settings.py
@@ -148,10 +148,10 @@ STATIC_ROOT = 'static/'
 
 
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_STORAGE_BUCKET_NAME = 'kitabkuber'
 AWS_S3_REGION_NAME = 'ap-south-1'
 
-SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = False
