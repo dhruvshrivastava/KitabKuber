e3c8d45adca8cca1612425af6b545b0f757b9d1b
Dhruv Shrivastava <dhruvsri5@gmail.com>
2020-12-01 12:21:50 +0530

    Update settings.py

--- kitabkuber/settings.py
+++ kitabkuber/settings.py
@@ -106,6 +106,8 @@ DATABASES = {
 
 }
 
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
