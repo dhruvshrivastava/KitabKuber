4b49e123bfae9a4d596765077be4ba239fd7729d
Dhruv Shrivastava <dhruvsri5@gmail.com>
2020-12-18 18:31:08 +0530

    Update admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from catalog.models import Books, Orders, Sell
+from catalog.models import Books, Orders, Sell, Enquiry
 
 admin.site.register(Books)
 admin.site.register(Orders)
 admin.site.register(Sell)
+admin.site.register(Enquiry)
